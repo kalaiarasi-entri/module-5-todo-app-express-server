feff4e128319b6642dbebfb5545de8a405a3832e
kalaiarasi-entri <kalaiarasientri@gmail.com>
2025-09-17 22:03:03 +0530

    url changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 const PORT = 8081;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://module-5-task-manager-react-client.netlify.app', credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
